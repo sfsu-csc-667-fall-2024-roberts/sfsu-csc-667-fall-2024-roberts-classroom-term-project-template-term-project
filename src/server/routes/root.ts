aadd72ce5553b5a5e78c2dfb5585c23430ba67aa
anavailableuser <112730974+anavailableuser@users.noreply.github.com>
2024-10-25 21:37:07 -0700

    fixes to repo

--- src/server/routes/root.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (_request, response) => {
-  response.render("root", { title: "Team P's site" });
-});
-
-export default router;
